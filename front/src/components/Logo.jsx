85377b69ae85863ed0dbd3a3e1828da799268367
STAP16 <stepakot0@gmail.com>
2025-09-14 16:42:32 +0300

    adding new logo + new tasks

--- front/src/components/Logo.jsx
+++ front/src/components/Logo.jsx
@@ -1,9 +1,9 @@
-import logo from '../assets/kotolog.jpg'
+import logo from '../assets/kotodo.jpg'
 
 export default function Logo(){
     return (
         <div className="logo">
-            <img src={logo}alt="kotolog" className='logo' />
+            <img src={logo}alt="kotodo" className='logo' />
         </div>
     )
 }
